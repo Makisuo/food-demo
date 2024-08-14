533bb320c96d0fdd3f8dfb10e63a26e8ffecdc3b
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-08-14 12:37:17 +0200

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 import { Heading } from "@pixelshades/ui/components"
-import { Suspense } from "react"
-import { getRandomRecipe } from "~/utils/api"
-import { FoodCard } from "./food-card"
 
 export default async function Home() {
-	const meal = getRandomRecipe()
-
 	return (
 		<div className="mx-auto w-full space-y-layout-sm py-layout-sm">
 			<Heading>Food Demo App</Heading>
-			<Suspense fallback={<div>Loading...</div>}>
-				<FoodCard mealPromise={meal} />
-			</Suspense>
+			<p>Build here</p>
 		</div>
 	)
 }
