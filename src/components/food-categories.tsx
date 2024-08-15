143cf2075fed378eb4467135293915163c1a269f
Makisuo <31933546+Makisuo@users.noreply.github.com>
2024-08-15 14:03:33 +0200

    fix layout

--- src/components/food-categories.tsx
+++ src/components/food-categories.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link"
+import { Link } from "@pixelshades/ui/components"
 import { getCategories } from "~/utils/api"
 
 export const FoodCategories = async () => {
 	const categories = await getCategories()
 
 	return (
-		<div>
+		<div className="flex flex-col gap-2">
 			{categories.map((category) => (
 				<Link key={category.idCategory} href={`/category/${category.idCategory}`}>
 					{category.strCategory}
